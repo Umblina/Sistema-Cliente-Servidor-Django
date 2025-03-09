414a62ef8c0deb5ec18c085910c1265bd1b584a6
Soylina <marialinab48@gmail.com>
2025-03-09 03:11:27 -0500

    Restaurando archivos eliminados

---
+++ tasks/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+# tasks/models.py
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)  # Campo de descripción
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
