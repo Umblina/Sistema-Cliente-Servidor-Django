194364ba13d5c16baf40d37aa06798fea605c5cd
Soylina <marialinab48@gmail.com>
2025-03-09 03:35:41 -0500

    Eliminando todos los archivos del control de Git

--- album/apps/fotos/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [ 
-    path("visor", views.fotos, name="fotos"),
-    ]
--- album/apps/fotos/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Mifoto
-
-
-def fotos(request):
-    mis_fotos = Mifoto.objects.all()
-    return render(request,"pages/fotos.html",{"fotos":mis_fotos})
